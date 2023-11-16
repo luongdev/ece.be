7685b87add1b195cb04fc4bc7648d94ac3dd3cce
Quangloi0727 <nguyenquangloi96@gmail.com>
2023-11-16 16:03:39 +0700

    remove try catch

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,14 +9,9 @@ export class AuthController {
 
     @Post('refresh-token')
     async refreshAccessToken(@Req() req, @Body() { refreshToken }: { refreshToken: string; }): Promise<any> {
-        try {
-            const username = req?.user?.username;
-            const { newAccessToken, newRefreshToken } = await this._authService.refreshAccessToken(refreshToken, username);
+        const username = req?.user?.username;
+        const { newAccessToken, newRefreshToken } = await this._authService.refreshAccessToken(refreshToken, username);
 
-            return { token: newAccessToken, refreshToken: newRefreshToken };
-        } catch (error) {
-
-            return error.message;
-        }
+        return { token: newAccessToken, refreshToken: newRefreshToken };
     }
 }
