3b154aae8d4cb08ca2954bc6e737b589b6898712
loinq <loinq@metechvn.com>
2024-01-05 16:54:00 +0700

    update ldap use username and password from input body

--- src/shared/providers/ldap/ldap-provider.service.ts
+++ src/shared/providers/ldap/ldap-provider.service.ts
@@ -7,29 +7,30 @@ import { LdapConfigService } from './ldap-config.service';
 
 @Injectable()
 export class LdapService extends PassportStrategy(Strategy, 'ldap') {
-
     constructor(
         private readonly _configService: ConfigService,
     ) {
-        super(LdapService.createOptions(_configService), async (req: Request, user: any, done) => {
+        super(({ body: { username, password } }, next) => {
+            next(null, LdapService.createOptions(_configService, username, password));
+        }, async (req: Request, user: any, done) => {
             req['user'] = user;
             user.username = req.body.username;
             return done(null, user);
         });
     }
 
-    private static createOptions(configService: ConfigService): any {
+    private static createOptions(configService: ConfigService, username?: string, password?: string): any {
         const ldapConfigService = new LdapConfigService(configService);
         const url = ldapConfigService.url;
         const bindDN = ldapConfigService.bindDN;
-        const bindCredentials = ldapConfigService.bindCredentials;
         const searchBase = ldapConfigService.searchBase;
+
         return {
             passReqToCallback: true,
             server: {
                 url: url,
-                bindDN: bindDN,
-                bindCredentials: bindCredentials,
+                bindDN: bindDN.replace('${username}', username),
+                bindCredentials: password,
                 searchBase: searchBase,
                 searchFilter: '(cn={{username}})',
                 searchAttributes: ['uid', 'displayName', 'mail']
@@ -37,6 +38,3 @@ export class LdapService extends PassportStrategy(Strategy, 'ldap') {
         };
     }
 }
-
-
-
